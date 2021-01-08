bcf6f09606bf029669e8f7345eca6c879ce61d51
treflebonbon <treflebonbon@gmail.com>
2021-01-08 14:40:13 +0900

    feat: TypeScript, ESLint, Prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,17 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
   ],
 }
